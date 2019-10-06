2a3be9b054cd0ad61ffdf47baf8a824196f6828d
moonbl3da <55296727+moonbl3da@users.noreply.github.com>
2019-10-06 16:39:03 +0200

    Create drill_5.cpp

---
+++ week_04/drill_5.cpp
@@ -0,0 +1,105 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int main(){
+
+//cout<<"Success!\n"; //1.
+
+//cout<<"Success!\n"; //2.
+
+//cout<<"Success"<<"!\n"; //3.
+
+//cout<<"success"<<'\n'; //4.
+
+/*string res = "7";
+vector<string> v(10);
+v[5] = res;
+cout << "Success!\n";*/ //5.
+
+/*vector<int> v(10);
+v[5] = 7;
+if (v[5]!=7);
+cout << "Success!\n";*/ //6.
+
+/*bool cond = true;
+if (cond) cout << "Success!\n";
+ .else cout << "Fail!\n";*/  //7.
+
+ /*bool c = true;
+  if (c) cout << "Success!\n";
+  else cout << "Fail!\n";*/ //8.
+
+  /*string s = "ape";
+  bool c = true;
+  if (c) cout << "Success!\n";*/ //9.
+
+  /*string s = "fool";
+  if (s=="fool")
+   cout << "Success!\n";*/ //10.
+
+   /*string s = "ape";
+   if (s=="ape")
+   cout <<"Success!\n";*/ // 11.
+
+   /*string s = "ape";
+   if (s=="fool")
+   cout << "Success!\n";*/ //12.
+
+  /* vector<char> v(5);
+    for (int i=0; 0<v.size(); ++i)
+    cout << "Success!\n"; */ //13.
+
+  /*  vector<char> v(5);
+    for (int i=0; i<=v.size(); ++i)
+     cout << "Success!\n"; */ //14.
+
+    /* string s = "Success!\n";
+     for (int i=0; i<s.size(); ++i) cout << s[i];*/ //15.
+
+     /*if (true) cout << "Success!\n";
+      else cout << "Fail!\n";*/ // 16.
+
+    /*  int x = 2000;
+      int c = x;
+      if (c==2000) cout << "Success!\n"; */ //17.
+
+      /*string s = "Success!\n";
+      for (int i=0; i<s.size(); ++i) cout << s[i];*/ //18.
+
+      /*vector<int> v(5);
+      for (int i=0; i<=v.size(); ++i)
+      cout << "Success!\n";*/ //19.
+
+    /*  int i=0; int j = 9;
+      while (i<10){ ++j;
+      if (j<i) cout << "Success!\n";
+    } */ //20.
+
+  /*  int x = 2;
+    double d = 4.5;
+    if (d==2*x+0.5)
+    cout << "Success!\n"; */ //21.
+
+    /* string s = "Success!\n";
+    for (int i=0; i<=10; ++i)
+    cout << s[i]; */ //22.
+
+    /* int i=0,j=0;
+     while (i<10){ ++j;
+     if (j<i) cout << "Success!\n";
+   }*/ // 23.
+
+   /*int x = 4;
+   double d = 5.0/(x-2);
+   if (d==((x-2)+0.5)){
+     cout << "Success!\n";
+   }
+   else {
+     cout<<"Fail!";
+   } */ //24.
+
+   /* cout << "Success!\n"; */ //25.
+
+   return 0;
+}
