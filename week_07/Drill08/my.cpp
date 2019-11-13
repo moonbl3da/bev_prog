6624dc55ed2cb49c2f9efdb4a50c35ceb75a9255
moonbl3da <55296727+moonbl3da@users.noreply.github.com>
2019-11-13 17:25:45 +0100

    Create my.cpp

---
+++ week_07/Drill08/my.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+#include "my.h"
+
+using namespace std;
+
+int foo;
+
+void print_foo(){
+	cout<<foo<<'\n';
+}
+
+void print(int i){
+	cout<<i<<'\n';
+}
+
+int main(){
+return 0;
+}
